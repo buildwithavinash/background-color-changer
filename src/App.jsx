9f8a043f4cefb4b7b579c92763d3ae156f3f1ef6
Avinash <buildwithavinash@users.noreply.github.com>
2026-03-12 11:58:25 +0530

    used useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useState, useEffect } from "react";
 import Button from "./components/Button";
 
+
 const App = () => {
+  const [color, setColor] = useState("white")
+
   let buttons = ["red", "green", "yellow", "blue", "purple"];
+
+   useEffect(()=>{
+        document.body.style.backgroundColor = color
+    }, [color])
+
+
   return (
     <div className="h-screen flex items-center justify-center flex-col gap-4">
       <div>
@@ -10,7 +20,7 @@ const App = () => {
 
       <div className="flex gap-4">
         {buttons.map((btn) => (
-          <Button key={btn} button={btn} />
+          <Button key={btn} button={btn} color={color} setColor={setColor} />
         ))}
       </div>
     </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,10 @@
-const Button = ({ button }) => {
-  function handleChange() {
-    document.body.style.backgroundColor = button;
-  }
+
+const Button = ({ button, setColor }) => {
+  
+   
+    const handleChange = () => {
+        setColor(button)
+    }
   return (
     <button
       className="border border-black px-1.5 py-1 cursor-pointer font-semibold text-slate-900"
